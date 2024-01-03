bb31755744a7d332a9eacfdd754e7b9547c429be
Allan Wu <allanwu@AllandeMac-mini.lan>
2024-01-03 23:50:47 +0800

    1、启用静态路由

--- src/views/login/index.vue
+++ src/views/login/index.vue
@@ -6,12 +6,17 @@ import { loginRules } from "./utils/rule";
 import { useNav } from "@/layout/hooks/useNav";
 import type { FormInstance } from "element-plus";
 import { useLayout } from "@/layout/hooks/useLayout";
-import { useUserStoreHook } from "@/store/modules/user";
-import { initRouter, getTopMenu } from "@/router/utils";
+// import { useUserStoreHook } from "@/store/modules/user";
+// import { initRouter, getTopMenu } from "@/router/utils";
+import { getTopMenu } from "@/router/utils";
 import { bg, avatar, illustration } from "./utils/static";
 import { useRenderIcon } from "@/components/ReIcon/src/hooks";
 import { ref, reactive, toRaw, onMounted, onBeforeUnmount } from "vue";
 import { useDataThemeChange } from "@/layout/hooks/useDataThemeChange";
+
+import { setToken } from "@/utils/auth";
+import { addPathMatch } from "@/router/utils";
+import { usePermissionStoreHook } from "@/store/modules/permission";
 
 import dayIcon from "@/assets/svg/day.svg?component";
 import darkIcon from "@/assets/svg/dark.svg?component";
@@ -42,21 +47,36 @@ const onLogin = async (formEl: FormInstance | undefined) => {
   if (!formEl) return;
   await formEl.validate((valid, fields) => {
     if (valid) {
-      useUserStoreHook()
-        .loginByUsername({ username: ruleForm.username, password: "admin123" })
-        .then(res => {
-          if (res.success) {
-            // 获取后端路由
-            initRouter().then(() => {
-              router.push(getTopMenu(true).path);
-              message("登录成功", { type: "success" });
-            });
-          }
-        });
+      setToken({
+        username: "admin",
+        roles: ["admin"],
+        accessToken: "eyJhbGciOiJIUzUxMiJ9.admin"
+      } as any);
+      // 全部采取静态路由模式
+      usePermissionStoreHook().handleWholeMenus([]);
+      addPathMatch();
+      router.push("/");
+      message("登录成功", { type: "success" });
     } else {
       loading.value = false;
       return fields;
     }
+    // if (valid) {
+    //   useUserStoreHook()
+    //     .loginByUsername({ username: ruleForm.username, password: "admin123" })
+    //     .then(res => {
+    //       if (res.success) {
+    //         // 获取后端路由
+    //         initRouter().then(() => {
+    //           router.push(getTopMenu(true).path);
+    //           message("登录成功", { type: "success" });
+    //         });
+    //       }
+    //     });
+    // } else {
+    //   loading.value = false;
+    //   return fields;
+    // }
   });
 };
 
