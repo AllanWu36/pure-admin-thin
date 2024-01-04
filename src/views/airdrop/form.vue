243bf438d031f25a182841060717362f7410eea6
Allan Wu <allanwu@AllandeMac-mini.lan>
2024-01-04 23:40:58 +0800

    1、添加表格

---
+++ src/views/airdrop/form.vue
@@ -0,0 +1,61 @@
+<script setup lang="ts">
+import { onMounted, ref } from "vue";
+import axios from "axios";
+
+// 声明 props 类型
+export interface FormProps {
+  formInline: {
+    user: string;
+    region: string;
+  };
+}
+
+// 声明 props 默认值
+// 推荐阅读：https://cn.vuejs.org/guide/typescript/composition-api.html#typing-component-props
+const props = withDefaults(defineProps<FormProps>(), {
+  formInline: () => ({ user: "", region: "" })
+});
+
+// vue 规定所有的 prop 都遵循着单向绑定原则，直接修改 prop 时，Vue 会抛出警告。此处的写法仅仅是为了消除警告。
+// 因为对一个 reactive 对象执行 ref，返回 Ref 对象的 value 值仍为传入的 reactive 对象，
+// 即 newFormInline === props.formInline 为 true，所以此处代码的实际效果，仍是直接修改 props.formInline。
+// 但该写法仅适用于 props.formInline 是一个对象类型的情况，原始类型需抛出事件
+// 推荐阅读：https://cn.vuejs.org/guide/components/props.html#one-way-data-flow
+const newFormInline = ref(props.formInline);
+
+const cities = ref([{ value: "北京", label: "北京" }]);
+onMounted(async () => {
+  try {
+    const response = await axios.get("http://localhost:8001/getlist");
+    cities.value = response.data; // 假设返回的数据是一个城市数组
+  } catch (error) {
+    console.error("获取城市数据失败", error);
+  }
+});
+</script>
+
+<template>
+  <el-form :model="newFormInline">
+    <el-form-item label="姓名">
+      <el-input
+        v-model="newFormInline.user"
+        class="!w-[220px]"
+        placeholder="请输入姓名"
+      />
+    </el-form-item>
+    <el-form-item label="城市">
+      <el-select
+        v-model="newFormInline.region"
+        class="!w-[220px]"
+        placeholder="请选择城市"
+      >
+        <el-option
+          v-for="city in cities"
+          :key="city.value"
+          :label="city.label"
+          :value="city.value"
+        />
+      </el-select>
+    </el-form-item>
+  </el-form>
+</template>
--- src/views/airdrop/index.vue
+++ src/views/airdrop/index.vue
@@ -17,23 +17,61 @@
         <el-button link type="primary" size="small" @click="handleClick(row)">
           详情
         </el-button>
-        <el-button link type="primary" size="small">修改图片</el-button>
+        <el-button
+          link
+          type="primary"
+          size="small"
+          @click="onFormOneClick(row)"
+        >
+          修改图片
+        </el-button>
         <el-button link type="warning" size="small">删除</el-button>
       </template>
     </pure-table>
+    <dialog />
   </div>
 </template>
 
 <script lang="ts" setup>
 import { ref, reactive } from "vue";
 import type { TableColumns } from "@pureadmin/table";
-
+import { addDialog } from "@/components/ReDialog";
+import forms, { type FormProps } from "./form.vue";
 function handleClick(row) {
   console.log(
     "%crow===>>>: ",
     "color: MidnightBlue; background: Aquamarine; font-size: 20px;",
     row
   );
+}
+
+// 结合Form表单（第一种方式，弹框关闭立刻恢复初始值）通过 props 属性接收子组件的 prop 并赋值
+function onFormOneClick(row) {
+  addDialog({
+    width: "70%",
+    title: "结合Form表单（第一种方式）",
+    contentRenderer: () => forms,
+    props: {
+      // 赋默认值
+      formInline: {
+        user: "菜虚鲲",
+        region: ""
+      }
+    },
+    closeCallBack: ({ options, args }) => {
+      // options.props 是响应式的
+      const { formInline } = options.props as FormProps;
+      const text = `姓名：${formInline.user} 城市：${formInline.region}`;
+      if (args?.command === "cancel") {
+        // 您点击了取消按钮
+        // message(`您点击了取消按钮，当前表单数据为 ${text},id为${row}`);
+      } else if (args?.command === "sure") {
+        // message(`您点击了确定按钮，当前表单数据为 ${text}`);
+      } else {
+        // message(`您点击了右上角关闭按钮或者空白页，当前表单数据为 ${text}`);
+      }
+    }
+  });
 }
 
 const columns: Array<TableColumns> = [
